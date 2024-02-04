a5474b42277253946264f7162398dd31b18d82ba
Cristian Barbon <98928287+barbonto@users.noreply.github.com>
2024-02-04 16:07:24 -0500

    Update main.py
    modificar el vector prediction para incluir los valores delta,
    cambiar el retorno para retornar una lista de valores

--- taller1/main.py
+++ taller1/main.py
@@ -6,22 +6,22 @@ Created on Wed Jan 31 20:04:37 2024
 """
 # 1. Library imports
 from fastapi import FastAPI
-from servicios.peng_model import penguin_data, PenguinModel
+from peng_model import penguin_data, PenguinModel
 
 # 2. Create app and model objects
 app = FastAPI()
 model = PenguinModel()
+
 # 3. Expose the prediction functionality, make a prediction from the passed
 #    JSON data and return the predicted flower species with the confidence
 @app.post('/predict')
 def predict_species(penguin: penguin_data):
-    data = penguin.model_dump()
+    data = penguin.dict()
+    prediction = model.predict_species(
+        data['culmen_length_mm'], data['culmen_depth_mm'], data['flipper_length_mm'],
+        data['body_mass_g'], data['sex'],data['delta15N'], data['delta13C'])
     
-    prediction, probability = model.predict_species(
-        data['island'], data['culmen_length_mm'], data['culmen_depth_mm'], data['flipper_length_mm'],
-        data['body_mass_g'], data['sex']
-    )
     return {
-        'prediction': prediction,
-        'probability': probability
+        'prediction': prediction.tolist()
+        #'probability': probability
     }
