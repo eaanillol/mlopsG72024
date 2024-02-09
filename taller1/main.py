49281caa2b92204eb1b9b68d77fb9a1f60647c63
Cristian Barbon <98928287+barbonto@users.noreply.github.com>
2024-02-08 23:02:27 -0500

    Update main.py
    inclusion de la variable penguin.Model_select  para cargar la Clase PenguinModel para elegir el modelo a utilizar en funcíon de esa variable

--- taller1/main.py
+++ taller1/main.py
@@ -10,17 +10,17 @@ from servicios.peng_model import penguin_data, PenguinModel
 
 # 2. Create app and model objects
 app = FastAPI()
-model = PenguinModel()
 
 # 3. Expose the prediction functionality, make a prediction from the passed
 #    JSON data and return the predicted flower species with the confidence
 @app.post('/predict')
-def predict_species(penguin: penguin_data):
+def predict_species(penguin: penguin_data): 
+    
+    model = PenguinModel(penguin.Model_select)
     data = penguin.model_dump()
     prediction = model.predict_species(
         data['culmen_length_mm'], data['culmen_depth_mm'], data['flipper_length_mm'],
         data['body_mass_g'], data['sex'],data['delta15N'], data['delta13C'])
-    
     return {
         'prediction': prediction
         #'probability': probability
